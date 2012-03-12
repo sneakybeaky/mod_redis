c234ea1e8178030f1adc29dab19ac2ed619f17b7
Jon Barber <jon.barber@acm.org>
2012-03-12 19:16:37 +0000

    Added JSON test for 15 additions to sorted set

--- tests/test_sorted_set.py
+++ tests/test_sorted_set.py
@@ -20,3 +20,18 @@ class TestSortedSet(test_mod_redis.TestModRedis):
         
         self.connection.request("GET","/redis/testset%(randomValue)d/count" % {"randomValue":randomValue})
         self.assertXmlResponse(self.connection.getresponse(),"integer","15")
+
+    def testAddFifteenItemsJson(self):
+    
+        randomValue = random.randint(0,sys.maxint)
+
+        headers = {"Content-type": "application/x-www-form-urlencoded"}
+        
+        for i in range(15):
+            self.connection.request("PUT","/redis/testset%(randomValue)d/user%(user)d.json" % {"randomValue":randomValue,"user":i} ,"%s" % i,headers)            
+
+            self.assertJsonResponse(self.connection.getresponse(),"integer","1")
+
+        
+        self.connection.request("GET","/redis/testset%(randomValue)d/count.json" % {"randomValue":randomValue})
+        self.assertJsonResponse(self.connection.getresponse(),"integer","15")
