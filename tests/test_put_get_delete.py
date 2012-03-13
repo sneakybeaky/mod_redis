60ece67893235a01d1e5ea9de70dc773af95a246
Jon Barber <jon.barber@acm.org>
2012-03-13 09:30:45 +0000

    Added test for create

--- tests/test_put_get_delete.py
+++ tests/test_put_get_delete.py
@@ -9,9 +9,21 @@ class TestPutGetDelete(test_mod_redis.TestModRedis):
 
     def testCRUDXml(self):
         testKey = "testcrudkey%d" % (self.getNextCounterValue())
+        expectedValue = "expectedValue"
 
+        # Make sure key is empty
         self.connection.request("GET","/redis/%(testKey)s" % {"testKey":testKey})
         self.assertXmlResponseIsNil(self.connection.getresponse())
+
+        # Populate the key
+        headers = {"Content-type": "application/x-www-form-urlencoded"}
+        self.connection.request("PUT","/redis/%(testKey)s" % {"testKey":testKey},expectedValue,headers)
+        self.assertXmlResponse(self.connection.getresponse(),"status","OK")
+
+        
+        
+
+        
 
         
 
