acbbd761b2905c102d57e29fe1e1eff3deb97990
Jon Barber <jon.barber@acm.org>
2012-03-13 09:35:27 +0000

    Added test for updating the key

--- tests/test_put_get_delete.py
+++ tests/test_put_get_delete.py
@@ -9,7 +9,8 @@ class TestPutGetDelete(test_mod_redis.TestModRedis):
 
     def testCRUDXml(self):
         testKey = "testcrudkey%d" % (self.getNextCounterValue())
-        expectedValue = "expectedValue"
+        expectedValue = "expectedValue"        
+        expectedValue2 = "expectedValue2"
 
         # Make sure key is empty
         self.connection.request("GET","/redis/%(testKey)s" % {"testKey":testKey})
@@ -22,7 +23,16 @@ class TestPutGetDelete(test_mod_redis.TestModRedis):
 
         # Read the key
         self.connection.request("GET","/redis/%(testKey)s" % {"testKey":testKey})
-        self.assertXmlResponse(self.connection.getresponse(),"string","expectedValue")
+        self.assertXmlResponse(self.connection.getresponse(),"string",expectedValue)
+
+        # Update the key
+        headers = {"Content-type": "application/x-www-form-urlencoded"}
+        self.connection.request("PUT","/redis/%(testKey)s" % {"testKey":testKey},expectedValue2,headers)
+        self.assertXmlResponse(self.connection.getresponse(),"status","OK")
+
+        # Read the key
+        self.connection.request("GET","/redis/%(testKey)s" % {"testKey":testKey})
+        self.assertXmlResponse(self.connection.getresponse(),"string",expectedValue2)
 
         # Delete the key
         self.connection.request("DELETE","/redis/%(testKey)s" % {"testKey":testKey})
